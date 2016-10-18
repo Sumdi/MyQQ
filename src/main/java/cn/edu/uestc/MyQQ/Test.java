7bf8001d8f93f283ed08654d90e1fa27b820d846
sumdi <sumdi@gmail.com>
2016-10-18 22:35:51 +0800

    the second commit by sumdi

---
+++ src/main/java/cn/edu/uestc/MyQQ/Test.java
@@ -0,0 +1,13 @@
+/**
+ * 版权声明。。。。。。。2016年10月18日
+ * ╭(╯^╰)╮	
+ */
+package cn.edu.uestc.MyQQ;
+
+/**
+ * @author 徐阳 2016年10月18日下午9:59:46
+ * @email xuyang8512@foxmail.com
+ */
+public class Test {
+
+}
