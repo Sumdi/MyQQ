9df8e9af122aeffa4059f7523da58c99d9558bf5
soxmomomo <evamore121@hotmail.com>
2016-10-19 23:10:12 +0800

    the third commit by mohanyi

---
+++ src/main/java/cn/edu/uestc/MyQQ/Test02.java
@@ -0,0 +1,16 @@
+package cn.edu.uestc.MyQQ;
+
+/**
+ * 
+ * ╮（￣▽￣）╭ 
+ * @author mohanyi
+ *
+ */
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("to test if I could push this file to git...");
+	}
+
+}
